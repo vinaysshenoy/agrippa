24a4216aaefed12eae8f11632383d365e4f8120e
Vinay Shenoy <vinay@doowup.com>
2016-10-29 18:30:10 +0530

    Fix invalid layout import

--- app/src/main/java/com/vinaysshenoy/agrippa/sample/MainActivity.java
+++ app/src/main/java/com/vinaysshenoy/agrippa/sample/MainActivity.java
@@ -3,7 +3,6 @@ package com.vinaysshenoy.agrippa.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.vinaysshenoy.agrippa.R;
 
 public class MainActivity extends AppCompatActivity {
 
